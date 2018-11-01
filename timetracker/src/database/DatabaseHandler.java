bb23a985a924e979df419362bef006845f06eaf5
ZEDZIJC <3636711+Axner@users.noreply.github.com>
2018-11-01 23:30:16 +0100

    Update timetracker/src/database/DatabaseHandler.java
    Add database handler for DB transactions.

---
+++ timetracker/src/database/DatabaseHandler.java
@@ -0,0 +1,45 @@
+package database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class DatabaseHandler {
+	
+	private final String databaseURL;
+	
+	public DatabaseHandler(){
+		this.databaseURL = "jdbc:sqlite:resources/projects.db";
+	}
+	
+	private PreparedStatement getPreparedStatement(String statement) throws SQLException {
+		Connection connection = DriverManager.getConnection(this.databaseURL);
+		return connection.prepareStatement(statement);
+		
+	}
+	 
+	public void addProject(String name, Integer isActive) throws SQLException {
+		String sqlStatement = "INSERT INTO projects(project_id, name, is_active) VALUES(null,?,?)";
+		PreparedStatement statement = this.getPreparedStatement(sqlStatement);
+		statement.setString(1, name);
+		statement.setInt(2, isActive);
+		statement.executeUpdate();
+		}
+	
+	public void updateProjectStatus(Integer id, Integer isActive) throws SQLException {
+		String sqlStatement = "UPDATE projects SET is_active=(?) WHERE project_id=(?)";
+		PreparedStatement statement = this.getPreparedStatement(sqlStatement);
+		statement.setInt(1, isActive);
+		statement.setInt(2, id);
+		statement.executeUpdate();
+	}
+	
+	public void addTime(Integer projectID, Integer time) throws SQLException {
+		String sqlStatement = "INSERT INTO timelogs(project_id, log_id, time) VALUES(?,null,?)";
+		PreparedStatement statement = this.getPreparedStatement(sqlStatement);
+		statement.setInt(1, projectID);
+		statement.setInt(2, time);
+		statement.executeUpdate();
+	}
+}
